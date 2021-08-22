76c83096e6cc67e042c7e950e4c0b3cf05495de9
Albin Hasani <albinhasani@protonmail.com>
2021-08-22 14:54:48 +0200

    Handle command line arguments

--- euclidean-algo/src/main.rs
+++ euclidean-algo/src/main.rs
@@ -1,7 +1,25 @@
+use std::env;
+use std::str::FromStr;
+
 // compute the greatest common divisor of two integers
 fn main() {
-    println!("Hello, world!");
-    println!("{}", gcd(12, 18));
+    let mut numbers = Vec::new();
+
+    for arg in env::args().skip(1) {
+        numbers.push(u64::from_str(&arg).expect("error parsing argument"));
+    }
+
+    if numbers.len() == 0 {
+        eprintln!("Usage: gcd NUMBER ...");
+        std::process::exit(1);
+    }
+
+    let mut d = numbers[0];
+    for m in &numbers[1..] {
+        d = gcd(d, *m);
+    }
+
+    println!("The greatest common divisor of {:?} is {}", numbers, d);
 }
 
 fn gcd(mut n: u64, mut m: u64) -> u64 {
